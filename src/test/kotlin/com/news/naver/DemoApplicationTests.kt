3f977a1748f2be53000a5b160367abbc05ec57b8
sijunkim <papaya9349@naver.com>
2025-08-09 19:19:10 +0900

    initialize project with Spring Boot and R2DBC, add configuration files and dependencies

---
+++ src/test/kotlin/com/news/naver/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.news.naver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
